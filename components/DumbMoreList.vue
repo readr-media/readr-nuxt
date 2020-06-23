cc574e667c05e388fe84bd21381d8317d5c50df5
yeefun <szyeefun@gmail.com>
2020-06-23 19:27:13 +0800

    feat: add more section

---
+++ components/DumbMoreList.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="more-list">
+    <h3 v-if="topic">{{ topic }}</h3>
+    <div class="list-wrapper">
+      <ul>
+        <li v-for="post in posts" :key="post.id">
+          <article>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <picture>
+                <img :src="post.heroImage" alt="" />
+              </picture>
+              <div class="text-wrapper">
+                <h1>{{ post.title }}</h1>
+                <div class="date">{{ post.publishedAt }}</div>
+              </div>
+            </a>
+          </article>
+        </li>
+      </ul>
+
+      <div class="load-more-wrapper">
+        <button type="button">看更多</button>
+        <svg
+          fill="none"
+          height="8"
+          viewBox="0 0 13 8"
+          width="13"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m6.9622 7.67246c-.39166.39736-1.03274.39736-1.4244 0l-5.187961-5.26348c-.622955-.63202-.175226-1.701977.712201-1.701977l10.37596.000001c.8874 0 1.3351 1.069956.7122 1.701976z"
+            fill="#000928"
+          />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DumbMoreList',
+  props: {
+    topic: {
+      type: String,
+      default: '',
+    },
+    posts: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  color: #000928;
+  letter-spacing: 2.8px;
+  font-size: 24px;
+  font-weight: 900;
+  line-height: 1.5;
+  text-align: center;
+  margin-bottom: 18px;
+  @include media-breakpoint-up(md) {
+    font-size: 32px;
+    letter-spacing: 3.75px;
+  }
+}
+.list-wrapper {
+  background-color: #fff;
+  border-radius: 2px;
+  padding: 16px 15px 18px 15px;
+  @include media-breakpoint-up(md) {
+    padding: 26px 25px 29px 25px;
+  }
+}
+li + li {
+  margin-top: 18px;
+}
+a {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  user-select: none;
+  cursor: pointer;
+}
+picture {
+  display: block;
+  position: relative;
+  width: calc((100% - 16px) * 0.49);
+  // 0.33 = 0.49 * 0.67
+  padding-top: calc((100% - 16px) * 0.33);
+  border-radius: 2px;
+  overflow: hidden;
+  @include media-breakpoint-up(md) {
+    width: calc((100% - 16px) * 0.44);
+    // 0.29 = 0.44 * 0.67
+    padding-top: calc((100% - 16px) * 0.29);
+  }
+}
+img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.text-wrapper {
+  width: calc((100% - 16px) * 0.51);
+  @include media-breakpoint-up(md) {
+    width: calc((100% - 16px) * 0.56);
+  }
+}
+h1 {
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 1.5;
+  color: rgba(#000, 0.87);
+  word-wrap: break-word;
+  -webkit-line-clamp: 3;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+.date {
+  font-size: 13px;
+  color: rgba(#000, 0.66);
+  line-height: 1.4;
+  margin-top: 10px;
+}
+.load-more-wrapper {
+  text-align: center;
+  margin-top: 20px;
+  user-select: none;
+  cursor: pointer;
+  @include media-breakpoint-up(md) {
+    margin-top: 26px;
+  }
+}
+button {
+  color: #000928;
+  letter-spacing: 2.5px;
+  // to offset letter-spacing at the rightmost
+  margin-left: 1.75px;
+
+  font-size: 15px;
+  line-height: 1.5;
+  font-weight: 700;
+  margin-bottom: 8px;
+}
+svg {
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ constants/mock.js
@@ -0,0 +1,203 @@
+export const mockCollaborativeProjects = [
+  {
+    id: 1,
+    title: '為了武漢肺炎防疫的需要，你可以接受你的權益被限制嗎？',
+    heroImage:
+      'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+    endTime: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    progress: 37,
+    link: 'https://www.readr.tw/series/Wuhan-pneumonia',
+  },
+  {
+    id: 2,
+    title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+    heroImage:
+      'https://www.readr.tw/assets/images/830962f5f7f9b90372a60067f0545aea/830962f5f7f9b90372a60067f0545aea.jpg',
+    // endTime: 'Mon, 22 Jun 2020 10:00:00 GMT',
+    progress: 87,
+    link: 'https://www.readr.tw/series/2020president',
+  },
+  {
+    id: 3,
+    title: '不一樣的彩虹：全台小學晨光時間大解密',
+    heroImage:
+      'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+    endTime: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    // progress: 56,
+    link: 'https://www.readr.tw/series/overseastaiwanese',
+  },
+  {
+    id: 4,
+    // title: '不一樣的彩虹：全台小學晨光時間大解密',
+    heroImage:
+      'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+    endTime: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    progress: 56,
+    link: 'https://www.readr.tw/series/food-delivery',
+  },
+]
+
+export const mockEditorsChoicePosts = [
+  {
+    id: 1,
+    title: '記者來當外送員：開箱美食外送秘辛！記者來當外送員',
+    link: 'https://www.readr.tw/series/Wuhan-pneumonia',
+    image:
+      'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+  },
+  {
+    id: 2,
+    title: '不一樣的彩虹：全台小學晨光',
+    link: 'https://www.readr.tw/series/food-delivery',
+    image:
+      'https://www.readr.tw/assets/images/830962f5f7f9b90372a60067f0545aea/830962f5f7f9b90372a60067f0545aea.jpg',
+  },
+  {
+    id: 3,
+    title: '港版國安法',
+    link: 'https://www.readr.tw/series/2020president',
+    image:
+      'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+  },
+]
+
+export const mockMorePostsNews = {
+  tag: '時事',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 2,
+      title: '不一樣的彩虹：全台小學晨光時間大解密',
+      heroImage:
+        'https://www.readr.tw/assets/images/830962f5f7f9b90372a60067f0545aea/830962f5f7f9b90372a60067f0545aea.jpg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 3,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 4,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
+
+export const mockMorePostsPolitics = {
+  tag: '政治',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
+
+export const mockMorePostsRights = {
+  tag: '人權',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 2,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
+
+export const mockMorePostsEnv = {
+  tag: '環境',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 2,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
+
+export const mockMorePostsEdu = {
+  tag: '教育',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 2,
+      title: '不一樣的彩虹：全台小學晨光時間大解密',
+      heroImage:
+        'https://www.readr.tw/assets/images/830962f5f7f9b90372a60067f0545aea/830962f5f7f9b90372a60067f0545aea.jpg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 3,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
+
+export const mockMorePostsOthers = {
+  tag: '新鮮事',
+  posts: [
+    {
+      id: 1,
+      title: '【武漢肺炎】武漢肺炎知識家',
+      heroImage:
+        'https://www.readr.tw/assets/images/5e2ba0d484f8369089f185afcf8b3be4/5e2ba0d484f8369089f185afcf8b3be4.png',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 2,
+      title: '不一樣的彩虹：全台小學晨光時間大解密',
+      heroImage:
+        'https://www.readr.tw/assets/images/830962f5f7f9b90372a60067f0545aea/830962f5f7f9b90372a60067f0545aea.jpg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+    {
+      id: 3,
+      title: '港版國安法將成「一國一制」？字詞分析網友怎麼看',
+      heroImage:
+        'https://www.readr.tw/assets/images/6324f78e5ddc9774c5e14cb6f2dbc34f/6324f78e5ddc9774c5e14cb6f2dbc34f.jpeg',
+      publishedAt: 'Tue, 09 Jun 2020 10:00:00 GMT',
+    },
+  ],
+}
