07d503649ae28d594498ea2411cc5c92770102de
yeefun <szyeefun@gmail.com>
2020-07-13 17:03:03 +0800

    feat: set browser compatibility for css and js

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,6 +67,48 @@ module.exports = {
    ** Build configuration
    */
   build: {
+    babel: {
+      /**
+       * nuxt 預設的 babel preset 為 @nuxt/babel-preset-app
+       * 詳細設定可見 https://github.com/nuxt/nuxt.js/tree/dev/packages/babel-preset-app
+       */
+      presets({ envName }) {
+        /**
+         * 理想上，瀏覽器支援列表 CSS 與 JS 應共用一份（即 .browserslistrc）
+         * 但 babel 吃 .browserslistrc 時，defaults 值會不起作用
+         * 此 bug 詳細可見 https://github.com/babel/babel/issues/10867，預計在第 8 版會修復
+         * 若修復完，code 記得修改成：
+            const envTargets = {
+              client: {}
+            }
+         */
+        const envTargets = {
+          client: {
+            browsers: [
+              'defaults',
+              'not ie <= 11',
+              'Edge >= 18',
+              'Chrome >= 49',
+              'Firefox >= 71',
+              'Safari >= 12.1',
+              'iOS >= 10.0',
+              'Samsung >= 9.2',
+              'Opera >= 46',
+              'OperaMobile >= 12',
+            ],
+          },
+        }
+
+        return [
+          [
+            '@nuxt/babel-preset-app',
+            {
+              targets: envTargets[envName],
+            },
+          ],
+        ]
+      },
+    },
     /*
      ** You can extend webpack config here
      */
