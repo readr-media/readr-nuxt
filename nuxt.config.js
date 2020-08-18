49d61e2da3d36bcf060c6c361abdb4ef549b48d3
yeefun <szyeefun@gmail.com>
2020-08-18 18:47:33 +0800

    feat: add the favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,29 @@ Object.assign(module.exports, {
       ...metaOg,
       ...metaFb,
       ...metaTwitter,
-      { hid: 'theme-color', name: 'theme-color', content: '#04295e' },
+      { name: 'theme-color', content: '#04295e' },
+      { name: 'msapplication-TileColor', content: '#ebf02c' },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#04295e' },
     ],
     script: [
       {
@@ -70,7 +92,6 @@ Object.assign(module.exports, {
         `,
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     __dangerouslyDisableSanitizersByTagID: {
       'adobe-fonts': ['innerHTML'],
     },
