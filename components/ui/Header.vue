fbc639f85ff6c8675111c500c2872375a020bf64
yeefun <szyeefun@gmail.com>
2020-08-21 16:09:14 +0800

    feat(header): add the donation link

--- components/ui/Header.vue
+++ components/ui/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <UiReadrLogoOfHeader />
+    <a href="/donate" target="_blank">贊助我們</a>
   </header>
 </template>
 
@@ -12,8 +13,20 @@ export default {
 
 <style lang="scss" scoped>
 header {
-  padding-top: 9px;
-  padding-bottom: 9px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 9px 20px;
+  letter-spacing: 2.5px;
+  color: #000928;
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 1.5;
+  @include media-breakpoint-up(md) {
+    padding-left: 26px;
+    padding-right: 26px;
+    font-size: 18px;
+  }
   ::v-deep svg {
     width: 48px;
     margin-left: auto;
