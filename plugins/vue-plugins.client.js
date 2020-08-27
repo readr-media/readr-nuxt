9bee8e45501b0da5c7520d5ef30c2e884505e69b
yeefun <szyeefun@gmail.com>
2020-08-27 17:36:03 +0800

    feat: add the loading image

--- plugins/vue-plugins.client.js
+++ plugins/vue-plugins.client.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
+import imgLoading from '~/assets/loading.gif'
 
-Vue.use(VueLazyload)
+Vue.use(VueLazyload, {
+  loading: imgLoading,
+})
