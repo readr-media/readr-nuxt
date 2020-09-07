df1053877a47c35d72cdae0a99433f3230c9de2b
yeefun <szyeefun@gmail.com>
2020-09-07 19:02:51 +0800

    feat: judge a slug to construct a project url

--- plugins/link.js
+++ plugins/link.js
@@ -1,9 +1,13 @@
+const oldNewsProjectSlugs = require('@readr-media/old-news-project-slugs')
+
 function getHref({ type, id, slug } = {}) {
   switch (type) {
     case 1:
       return `/post/${id}`
     case 4:
-      return `/project/${slug}`
+      return oldNewsProjectSlugs.includes(slug)
+        ? `/project/${slug}`
+        : `/project/3/${slug}`
     default:
       return undefined
   }
