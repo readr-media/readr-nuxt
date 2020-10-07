f597a5a1d9ecbc8bd3f54cfcdf624e7d60962eab
kwhsiung <kaiwenhsiung@gmail.com>
2020-10-07 14:52:17 +0800

    chore: adjust comment of ignoredElements

--- plugins/web-components.client.js
+++ plugins/web-components.client.js
@@ -7,8 +7,7 @@ import {
   defineCustomElements,
 } from '@readr-media/web-components/loader'
 
-// Tell Vue to ignore all components defined in the test-components
-// package, we could provide regex here
+// Tell Vue to ignore the components. We can provide regex here
 Vue.config.ignoredElements = ['readr-header']
 
 // Bind the custom elements to the window object
