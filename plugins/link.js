b3894b76385af67aa2e138acb55ac2b3756cd824
yeefun <szyeefun@gmail.com>
2020-10-27 12:27:13 +0800

    feat: build a mirror media project href by the slug

--- plugins/link.js
+++ plugins/link.js
@@ -1,13 +1,20 @@
-const oldNewsProjectSlugs = require('@readr-media/old-news-project-slugs')
+const {
+  READR_MEDIA_OLD_PROJECT_SLUGS,
+  MIRROR_MEDIA_OLD_PROJECT_SLUGS,
+} = require('@readr-media/old-news-project-slugs')
 
 function href({ style, id, slug }) {
   switch (style) {
     case 'news':
       return `/post/${id}`
     case 'report':
-      return oldNewsProjectSlugs.includes(slug)
-        ? `/project/${slug}`
-        : `/project/3/${slug}`
+      if (READR_MEDIA_OLD_PROJECT_SLUGS.includes(slug)) {
+        return `/project/${slug}`
+      } else if (MIRROR_MEDIA_OLD_PROJECT_SLUGS.includes(slug)) {
+        return `https://www.mirrormedia.mg/projects/${slug}/index.html`
+      } else {
+        return `/project/3/${slug}`
+      }
     default:
       return undefined
   }
