cf732abfea96819c78630b6f14a9f21130f08924
kwhsiung <kaiwenhsiung@gmail.com>
2021-02-08 16:21:38 +0800

    feat(donate-page): copy donate page component from legacy and compile stylus to css

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,9 +1,72 @@
 <template>
-  <div></div>
+  <section class="app-donate">
+    <DonateForm
+      v-if="!showResult"
+      @submitForm="onFormSubmit"
+      @showResultSuccess="onShowResultSuccess"
+      @showResultFail="onShowResultFail"
+    />
+    <DonateResult
+      v-else
+      :isResultSuccess="isResultSuccess"
+      :formData="formData"
+      @backToForm="showResult = false"
+      @closeSidebar="$emit('closeSidebar')"
+    />
+  </section>
 </template>
 
 <script>
-export default {}
+import { SITE_NAME } from 'src/constants'
+import DonateForm from 'src/components/donate/DonateForm.vue'
+import DonateResult from 'src/components/donate/DonateResult.vue'
+
+export default {
+  name: 'AppDonate',
+  components: {
+    DonateForm,
+    DonateResult,
+  },
+  metaInfo: {
+    title: '贊助 READr',
+    meta: [
+      {
+        vmid: 'og:title',
+        property: 'og:title',
+        content: `贊助 READr - ${SITE_NAME}`,
+      },
+    ],
+  },
+  data() {
+    return {
+      showResult: false,
+      isResultSuccess: false,
+      formData: {
+        donateAmount: 0,
+        carrierTypeSelected: '',
+        carrierInputs: '',
+        date: '',
+      },
+    }
+  },
+  methods: {
+    onFormSubmit(formData) {
+      this.$set(this, 'formData', formData)
+    },
+    onShowResultSuccess() {
+      this.showResult = true
+      this.isResultSuccess = true
+    },
+    onShowResultFail() {
+      this.showResult = true
+      this.isResultSuccess = false
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app-donate {
+  padding: 0;
+}
+</style>
