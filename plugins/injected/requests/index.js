87c7f2b4e89d73d3868be9cfe922270dc89c5463
yeefun <szyeefun@gmail.com>
2021-03-19 16:00:03 +0800

    refactor: remove the unused plugin & package
    plugin: fetchLatestPosts
    package: qs

--- plugins/injected/requests/index.js
+++ plugins/injected/requests/index.js
@@ -1,10 +1,9 @@
-const { fetchLatestPosts, fetchPost, fetchPostsByTag } = require('./posts.js')
+const { fetchPost, fetchPostsByTag } = require('./posts.js')
 const { postDonate, postSubscribe } = require('./donate.js')
 
 module.exports = injectRequestMethods
 
 function injectRequestMethods(ctx, inject) {
-  inject('fetchLatestPosts', fetchLatestPosts)
   inject('fetchPost', fetchPost.bind(ctx))
   inject('fetchPostsByTag', fetchPostsByTag)
 
--- plugins/injected/requests/posts.js
+++ plugins/injected/requests/posts.js
@@ -1,5 +1,4 @@
-const { camelizeKeys, decamelizeKeys } = require('humps')
-const { stringify: qsStringify } = require('qs')
+const { camelizeKeys } = require('humps')
 const { create: createAxios, get: axiosGet } = require('axios')
 
 const { REQUEST_TIMEOUT } = require('~/configs/config.js')
@@ -11,34 +10,6 @@ const publicApi = createAxios({
   baseURL: `${baseUrl}/api/public`,
   timeout: REQUEST_TIMEOUT,
 })
-
-async function fetchPosts(params = {}) {
-  const requestUrl = `/posts${buildParams(params)}`
-
-  try {
-    const { data } = await publicApi.get(requestUrl)
-
-    return camelizeKeys(data).items || []
-  } catch (err) {
-    logPostsApiError(err)
-    return []
-  }
-}
-
-async function fetchLatestPosts(params = {}) {
-  return await fetchPosts({
-    type: '{"$in":[1,4]}',
-    maxResult: 5,
-    page: 1,
-    sort: '-published_at',
-    showAuthor: false,
-    showUpdater: false,
-    showTag: false,
-    showComment: false,
-    showProject: false,
-    ...params,
-  })
-}
 
 async function fetchPost(postId) {
   const requestUrl = `/post/${postId}`
@@ -74,20 +45,11 @@ async function fetchPostsByTag(tagId) {
   }
 }
 
-function buildParams(params = {}) {
-  if (Object.keys(params).length === 0) {
-    return ''
-  }
-
-  return `?${qsStringify(decamelizeKeys(params))}`
-}
-
 function logPostsApiError(err) {
   logApiError(err, 'Posts')
 }
 
 Object.assign(module.exports, {
-  fetchLatestPosts,
   fetchPostsByTag,
   fetchPost,
 })
