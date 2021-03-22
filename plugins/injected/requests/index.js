80a14ab34815f6c64eb68b80326327b1a9df39db
yeefun <szyeefun@gmail.com>
2021-03-22 19:08:22 +0800

    refactor: remove the unused fetchPostsByTag plugin

--- plugins/injected/requests/index.js
+++ plugins/injected/requests/index.js
@@ -1,11 +1,10 @@
-const { fetchPost, fetchPostsByTag } = require('./posts.js')
+const { fetchPost } = require('./posts.js')
 const { postDonate, postSubscribe } = require('./donate.js')
 
 module.exports = injectRequestMethods
 
 function injectRequestMethods(ctx, inject) {
   inject('fetchPost', fetchPost.bind(ctx))
-  inject('fetchPostsByTag', fetchPostsByTag)
 
   inject('postDonate', postDonate)
   inject('postSubscribe', postSubscribe)
--- plugins/injected/requests/posts.js
+++ plugins/injected/requests/posts.js
@@ -1,5 +1,5 @@
 const { camelizeKeys } = require('humps')
-const { create: createAxios, get: axiosGet } = require('axios')
+const { create: createAxios } = require('axios')
 
 const { REQUEST_TIMEOUT } = require('~/configs/config.js')
 const { logApiError } = require('~/helpers/index.js')
@@ -26,30 +26,10 @@ async function fetchPost(postId) {
   }
 }
 
-async function fetchPostsByTag(tagId) {
-  const requestUrl = `${baseUrl}/api/cms/deprecated/tags/pnr/${tagId}`
-
-  try {
-    const { data } = await axiosGet(requestUrl, {
-      params: {
-        max_result: 3,
-        sort: '-published_at',
-      },
-      timeout: REQUEST_TIMEOUT,
-    })
-
-    return camelizeKeys(data).items
-  } catch (err) {
-    logPostsApiError(err)
-    return []
-  }
-}
-
 function logPostsApiError(err) {
   logApiError(err, 'Posts')
 }
 
 Object.assign(module.exports, {
-  fetchPostsByTag,
   fetchPost,
 })
