0815985361b15201da295376d7bacd8eebef2633
yeefun <szyeefun@gmail.com>
2021-03-23 16:41:22 +0800

    refactor: remove the unused fetchPost plugin

--- plugins/injected/requests/index.js
+++ plugins/injected/requests/index.js
@@ -1,11 +1,8 @@
-const { fetchPost } = require('./posts.js')
 const { postDonate, postSubscribe } = require('./donate.js')
 
 module.exports = injectRequestMethods
 
 function injectRequestMethods(ctx, inject) {
-  inject('fetchPost', fetchPost.bind(ctx))
-
   inject('postDonate', postDonate)
   inject('postSubscribe', postSubscribe)
 }
