f02bbf3db7f6038096cc86857e0876c35d81462a
kwhsiung <kaiwenhsiung@gmail.com>
2021-03-26 10:50:23 +0800

    feat(china-popculture): add styles of hover state and adjust styles of toggle state of anseer text in quiz article

--- components/report/china-popculture/components/RdQuizArticleAnswerText.vue
+++ components/report/china-popculture/components/RdQuizArticleAnswerText.vue
@@ -40,11 +40,20 @@ export default {
 .underline {
   border-bottom: 1px solid #6a6a6a;
   cursor: pointer;
+  transition: background-color 0.1s ease-out, border-bottom 0.1s ease-out,
+    color 0.1s ease-out, font-weight 0.1s ease-out;
+  &:hover {
+    font-weight: 500;
+    background-color: #ffeded;
+    color: black;
+    border-bottom: 1px solid transparent;
+  }
 }
 
 .toggled {
-  background-color: #f50e0e;
-  color: white;
+  font-weight: 500;
+  background-color: #f50e0e !important;
+  color: white !important;
   border-bottom: 1px solid transparent !important;
 }
 </style>
