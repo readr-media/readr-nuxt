a7a64237bb05f236f85ab8889c2c09ff0a20a99d
yeefun <szyeefun@gmail.com>
2021-04-16 02:35:27 +0800

    style: remove empty lines

--- helpers/index.js
+++ helpers/index.js
@@ -3,11 +3,7 @@ import { ENV } from '../configs/config.js'
 const isProdEnv = ENV === 'prod'
 
 export { isProdEnv }
-
 export { reportApiErrorFromKoa, logApiError, getErrorName } from './error.js'
-
 export { setLocalStorage, getLocalStorage } from './local-storage.js'
-
 export { SITE_TITLE, SITE_URL } from './meta.js'
-
 export { getHref, formatDate } from './post.js'
