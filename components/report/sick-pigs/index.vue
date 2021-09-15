403244f69dfceaa19c5adacb739a264e1625b899
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-09-15 11:09:35 +0800

    feat: add index.vue and header/footer

---
+++ components/report/sick-pigs/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sick-pigs"><RdReportHeader class="header" />123</div>
+</template>
+
+<script>
+import RdReportHeader from '~/components/app/Report/RdReportHeader.vue'
+export default {
+  components: {
+    RdReportHeader,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.sick-pigs {
+  background: #dddddd;
+}
+</style>
+
+<style lang="scss">
+#default-footer {
+  background-color: #dddddd;
+}
+</style>
