2315ff6a9cc5228e1ad20acad25705dfa03967d1
kwhsiung <kaiwenhsiung@gmail.com>
2021-11-16 18:07:41 +0800

    feat(header): cache the response also if the ENV is staging

--- server-middleware/header.js
+++ server-middleware/header.js
@@ -1,7 +1,8 @@
+import { ENV } from '../configs/config.js'
 const { isProdEnv } = require('../helpers/index.js')
 
 function setHeader(_, res, next) {
-  if (isProdEnv) {
+  if (isProdEnv || ENV === 'staging') {
     res.setHeader('Cache-Control', 'private, max-age=300')
   } else {
     res.setHeader('Cache-Control', 'no-store')
