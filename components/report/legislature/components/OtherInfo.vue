022c9886a0ce641285be3f0f2137580c3be52975
v61265 <66636137+v61265@users.noreply.github.com>
2021-12-23 17:14:28 +0800

    feat(lesgisture): add otherInfo (#247)
    Co-authored-by: YiHsuan <yihsuanlee@mirrormedia.mg>

--- components/report/legislature/components/OtherInfo.vue
+++ components/report/legislature/components/OtherInfo.vue
@@ -1,5 +1,79 @@
 <template>
   <section>
     <!--閱讀測驗、如果你關心這個議題等等功能-->
+    <RdReportExtras
+      :contents="cmsData.contentApiData.extras.contents"
+      @sendGaEvent="sendGaEvent"
+    />
+    <RdReportCredit
+      :authors="cmsData.contentApiData.credit"
+      :publishedAt="cmsData.contentApiData.publishedAt"
+      :canSendGaEvent="true"
+    />
+    <Donate @clickButton="$ga.event('projects', 'click', 'donate')" />
   </section>
 </template>
+
+<script>
+// import RdQuiz from '~/components/RdQuiz.vue'
+import Donate from './Donate.vue'
+import RdReportExtras from '~/components/app/Report/RdReportExtras.vue'
+import RdReportCredit from '~/components/app/Report/RdReportCredit.vue'
+
+export default {
+  components: {
+    // RdQuiz,
+    RdReportExtras,
+    RdReportCredit,
+    Donate,
+  },
+  props: {
+    cmsData: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$primary-color: #33aba4;
+.report-extras::v-deep {
+  background: #1b1b1b;
+  color: #ffffff;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    padding-left: 1em;
+  }
+  li::before {
+    content: ' ';
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background: #fff;
+    // top: 0;
+  }
+  a {
+    color: $primary-color;
+  }
+}
+
+.report-credit {
+  background: #1b1b1b;
+  color: #ffffff;
+}
+
+.donate-button {
+  background: #1b1b1b;
+  padding: 24px 59px 28px 55px;
+
+  @media (min-width: 768px) {
+    padding: 60px;
+  }
+}
+</style>
