330f46b6ae2d43636b44975db08b70b4925015f2
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-12-27 15:17:55 +0800

    feat(legislature): add anchor-group

---
+++ components/report/legislature/components/Anchor.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="anchor" :class="{ active: isActive }">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.anchor {
+  padding: 4px 8px;
+  font-weight: 900;
+  font-size: 16px;
+  line-height: 23px;
+  color: #ffffff;
+  background: #33aba4;
+
+  & + & {
+    margin-left: 8px;
+  }
+}
+
+.active {
+  background: #979797;
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
--- components/report/legislature/components/Header.vue
+++ components/report/legislature/components/Header.vue
@@ -1,33 +1,68 @@
 <template>
-  <readr-header
-    @readrLogoLinkClick="sendGaClickEvent('back to home')"
-    @shareFacebookLinkClick="sendGaClickEvent('share to fb')"
-    @shareLineLinkClick="sendGaClickEvent('share to line')"
-  />
+  <div class="header">
+    <readr-header
+      @readrLogoLinkClick="sendGaClickEvent('back to home')"
+      @shareFacebookLinkClick="sendGaClickEvent('share to fb')"
+      @shareLineLinkClick="sendGaClickEvent('share to line')"
+    />
+    <div class="header__anchors">
+      <img src="../../../../assets/imgs/report/legislature/arrow.svg" />
+      <Anchor title="看立法院專題報導" />
+      <Anchor title="看法案資料" :isActive="true" />
+    </div>
+  </div>
 </template>
 
+<script>
+import Anchor from './Anchor.vue'
+export default {
+  components: {
+    Anchor,
+  },
+}
+</script>
+
 <style lang="scss" scoped>
-readr-header::v-deep {
-  background: #1b1b1b;
+.header {
   position: fixed;
   top: 0;
-  path {
-    fill: #fff;
-  }
+  z-index: 200;
+  background: #1b1b1b;
+  width: 100vw;
 
-  share-nav {
-    position: fixed;
-    right: 14px;
-    @include media-breakpoint-up(md) {
-      right: 20px;
+  readr-header::v-deep {
+    path {
+      fill: #fff;
     }
 
-    a {
-      circle {
-        fill: #fff;
+    share-nav {
+      a {
+        circle {
+          fill: #fff;
+        }
+        path {
+          fill: #33aba4;
+        }
       }
-      path {
-        fill: #33aba4;
+    }
+  }
+
+  &__anchors {
+    padding: 15px 0 24px 0;
+    margin: 0 20px;
+    border-bottom: 0.5px solid #ffffff;
+    display: flex;
+    @include media-breakpoint-up(md) {
+      border-bottom: 0;
+      position: absolute;
+      top: 12px;
+      right: 90px;
+    }
+    img {
+      margin-right: 8px;
+      display: none;
+      @include media-breakpoint-up(md) {
+        display: inherit;
       }
     }
   }
