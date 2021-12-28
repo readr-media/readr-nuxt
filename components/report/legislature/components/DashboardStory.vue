3bf9999d00e53448f8f600aeebec1064cbe6a7e0
YiHsuan <yihsuanlee@mirrormedia.mg>
2021-12-29 02:37:23 +0800

    feat(legislature): add page of dashboard-animation and add as iframe

--- components/report/legislature/components/DashboardStory.vue
+++ components/report/legislature/components/DashboardStory.vue
@@ -1,3 +1,18 @@
 <template>
-  <section></section>
+  <section>
+    <iframe src="/iframe/legislature" />
+  </section>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+iframe {
+  z-index: 3000;
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+}
+</style>
